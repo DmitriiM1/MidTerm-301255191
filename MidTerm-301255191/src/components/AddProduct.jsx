c34dc625c4000faf9f0638ca149d28a75e60bbb1
Dmitrii <114525458+DmitriiM1@users.noreply.github.com>
2024-06-19 00:00:17 +0000

    Created basic structure for two forms

---
+++ MidTerm-301255191/src/components/AddProduct.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import "./AddProduct.css";
+
+export default function AddProduct(){
+
+    
+}
---
+++ MidTerm-301255191/src/components/SignUpUser.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import "./SignUpUser.css";
+
+export default function SignUpUser(){
+    
+}
